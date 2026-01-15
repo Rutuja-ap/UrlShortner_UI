f3c8fbdc7018b11935d475e01d5ef7332af9abad
Rutuja Patil <rutujapatil5062@gmail.com>
2026-01-15 21:19:44 +0530

    Update API to use .env and clean src/.env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.APP_BACKEND_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 /* ---------- SINGLE URL ---------- */
 export const shortenUrl = async (originalUrl) => {
   return axios.post(
