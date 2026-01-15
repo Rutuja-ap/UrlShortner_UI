3a8941012f8b7e97c12f7b32aab088d391986b36
Rutuja Patil <rutujapatil5062@gmail.com>
2026-01-15 22:47:40 +0530

    backend reachable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 /* ---------- SINGLE URL ---------- */
 export const shortenUrl = async (originalUrl) => {
   return axios.post(
